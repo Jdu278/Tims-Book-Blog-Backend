da14dbee57356e5562522b11fa8d040ae0a6dd6c
Tim Schauder <temscha@live.com>
2023-07-17 15:04:59 +0200

    Add getAll and getOne Methods to book service

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Book } from './entities/book.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class BooksService {
-  getAllBooks() {
-    return `This action returns all books`;
+  constructor(
+    @InjectRepository(Book) private bookRepository: Repository<Book>,
+  ) {}
+
+  async getAllBooks() {
+    const allBooks = await this.bookRepository.find();
+    return allBooks;
   }
 
-  getOneBook(id: number) {
-    return `This action returns a #${id} book`;
+  async getOneBook(id: string): Promise<Book> {
+    const book = await this.bookRepository.findOne({
+      where: { id: id },
+    });
+    return book;
   }
 }
