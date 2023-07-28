8d2c23766a32b3fd4ba22423df524251421055bb
Tim Schauder <temscha@live.com>
2023-07-28 10:26:37 +0200

    Add test for getAllBooks and getOneBook

--- src/books/books.service.spec.ts
+++ src/books/books.service.spec.ts
@@ -1,5 +1,4 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { createMock } from '@golevelup/ts-jest';
 import { BooksService } from './books.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
@@ -9,11 +8,11 @@ const buildBook = () => {
   return {
     id: 'e6ceb44b-924b-4ae4-83f9-3a25ed69f628',
     title: 'Testtitle',
-    author: 'Testauthor',
-    year: 1980,
+    authors: 'Testauthor',
+    releaseDate: '22-04-1998',
     pages: 200,
     isbn: '234323432sdcvf',
-    publisher: 'Testpublisher',
+    publishers: 'Testpublisher',
     monthRead: 'January 2023',
     description: 'Testdescription',
     review: 'Testreview',
@@ -21,6 +20,7 @@ const buildBook = () => {
     image: 'Testlink',
     createdAt: new Date(),
     updatedAt: new Date(),
+    importId: '234',
   };
 };
 describe('BooksService', () => {
@@ -33,7 +33,10 @@ describe('BooksService', () => {
         BooksService,
         {
           provide: getRepositoryToken(Book),
-          useValue: createMock<Repository<Book>>,
+          useValue: {
+            find: jest.fn().mockResolvedValue([]),
+            findOne: jest.fn().mockResolvedValue([]),
+          },
         },
       ],
     }).compile();
@@ -42,20 +45,73 @@ describe('BooksService', () => {
     mockRepository = module.get<Repository<Book>>(getRepositoryToken(Book));
   });
 
+  describe('getAllBooks', () => {
+    it('should return all books', async () => {
+      // Given
+      const allBooks = [buildBook(), buildBook(), buildBook()];
+      const expectedResponse = {
+        books: [
+          {
+            importId: allBooks[0].importId,
+            title: allBooks[0].title,
+            authors: allBooks[0].authors,
+            pages: allBooks[0].pages,
+            publishers: allBooks[0].publishers,
+            releaseDate: allBooks[0].releaseDate,
+            image: allBooks[0].image,
+          },
+          {
+            importId: allBooks[1].importId,
+            title: allBooks[1].title,
+            authors: allBooks[1].authors,
+            pages: allBooks[1].pages,
+            publishers: allBooks[1].publishers,
+            releaseDate: allBooks[1].releaseDate,
+            image: allBooks[1].image,
+          },
+          {
+            importId: allBooks[2].importId,
+            title: allBooks[2].title,
+            authors: allBooks[2].authors,
+            pages: allBooks[2].pages,
+            publishers: allBooks[2].publishers,
+            releaseDate: allBooks[2].releaseDate,
+            image: allBooks[2].image,
+          },
+        ],
+      };
+
+      jest.spyOn(mockRepository, 'find').mockResolvedValue(allBooks);
+
+      // When
+      const response = await service.getAllBooks();
+
+      // Then
+      expect(response).toEqual(expectedResponse);
+      expect(mockRepository.find).toHaveBeenCalled();
+    });
+  });
   describe('getOneBook', () => {
-    // it('should return a single book by given id', async () => {
-    //   // Given
-    //   const expectedResponse = buildBook();
-    //   const bookId = expectedResponse.id;
-    //   jest.spyOn(mockRepository, 'findOne').mockResolvedValue(expectedResponse);
-
-    //   // When
-    //   const response = await service.getOneBook(bookId);
-    //   console.log(response)
-
-    //   // Then
-    //   expect(response).toBe(expectedResponse);
-    //   expect(mockRepository.findOne).toHaveBeenCalledWith(bookId);
-    // });
+    it('should return a single book by given id', async () => {
+      // Given
+      const book = buildBook();
+      const expectedResponse = {
+        importId: book.importId,
+        title: book.title,
+        authors: book.authors,
+        pages: book.pages,
+        publishers: book.publishers,
+        releaseDate: book.releaseDate,
+        image: book.image,
+      };
+      jest.spyOn(mockRepository, 'findOne').mockResolvedValue(book);
+      // When
+      const response = await service.getOneBook(book.id);
+      // Then
+      expect(response).toEqual(expectedResponse);
+      expect(mockRepository.findOne).toHaveBeenCalledWith({
+        where: { importId: book.id },
+      });
+    });
   });
 });
